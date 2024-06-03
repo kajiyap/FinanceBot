e137e033177d8f9bd9af79cccf66ba08e2771b1f
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2024-06-03 11:05:01 -0300

    Bot base com exemplo de função

---
+++ ia.py
@@ -0,0 +1,24 @@
+import pathlib
+import textwrap
+import google.generativeai as genai
+from googlefinance import getQuotes
+import json
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+
+genai.configure(api_key=GOOGLE_API_KEY)
+
+def multiply(a:float, b:float):
+    """returns a * b."""
+    return a*b
+
+model = genai.GenerativeModel('gemini-1.5-flash', tools=[multiply])
+chat = model.start_chat(enable_automatic_function_calling=True)
+
+def question(pergunta):
+    response = chat.send_message(pergunta)
+    return(response.text)
---
+++ main.py
@@ -0,0 +1,17 @@
+import telebot
+import ia
+
+import os
+from dotenv import load_dotenv, dotenv_values
+
+load_dotenv()
+
+telegramKey = os.getenv("TELEGRAM_API_KEY")
+
+bot = telebot.TeleBot(telegramKey, parse_mode=None)
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+	bot.reply_to(message, ia.question(message.text))
+
+bot.infinity_polling()
