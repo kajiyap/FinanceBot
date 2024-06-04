71d8d910dbe7a6d65993740680fde0327e0e9f24
Pedro Santos Kajiya <kajiyapedro@gmail.com>
2024-06-04 10:12:13 -0300

    Adiciona função para valor de cotação
    Co-Authored-By: Bruno Vieira <76965972+BrunoVieira003@users.noreply.github.com>

--- ia.py
+++ ia.py
@@ -1,8 +1,10 @@
-import pathlib
-import textwrap
+import datetime
+import pandas as pd
+import pandas_datareader
+import yfinance as yf
 import google.generativeai as genai
-import json
 import os
+import setuptools
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
@@ -11,11 +13,18 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
+def pegarCotas (nome: str, period: int=1):
+    ticker = yf.Ticker(nome)
+    try:
+        return ticker.history(period=f"{period}d")['Close'][0]
+    except IndexError as ex:
+        return 'Não foi possível encontrar essa sigla.'
+
 def multiply(a:float, b:float):
     """returns a * b."""
     return a*b
 
-model = genai.GenerativeModel('gemini-1.5-flash', tools=[multiply])
+model = genai.GenerativeModel('gemini-1.5-flash', tools=[multiply, pegarCotas])
 chat = model.start_chat(enable_automatic_function_calling=True)
 
 def question(pergunta):
