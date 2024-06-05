20b90b54ffe0500015886e8203df5caafb1edbd0
Dudaleite08 <duleite08@icloud.com>
2024-06-05 10:22:24 -0300

    Implementação de função para pegar índices da Ibovespa

--- ia.py
+++ ia.py
@@ -9,6 +9,15 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 genai.configure(api_key=GOOGLE_API_KEY)
 
+def pegarIbovespa():
+    try:
+        ticker = yf.Ticker('^BVSP')
+        resposta = ticker.info['previousClose']
+        if(resposta != ''):
+            return resposta
+    except (IndexError, KeyError) as ex:
+        resposta = 'Não foi possível encontrar o valor desse índice'
+
 def pegarValorCota(nome: str):
     suffix = ['', '.SA']
     resposta = ''
@@ -69,7 +78,7 @@ def pegarUltimoDividendo(nome: str):
 def panaromaGeral(nome: str):
     return f"Nome da empresa: {pegarNomeEmpresa(nome)}. País: {pegarPais(nome)}. Último dividendo: {pegarUltimoDividendo(nome)}. Cotação atual: {pegarValorCota(nome)}"
 
-model = genai.GenerativeModel('gemini-1.5-flash', tools=[pegarValorCota, pegarNomeEmpresa, pegarUltimoDividendo, pegarPais, panaromaGeral])
+model = genai.GenerativeModel('gemini-1.5-flash', tools=[pegarValorCota, pegarNomeEmpresa, pegarUltimoDividendo, pegarPais, panaromaGeral, pegarIbovespa])
 chat = model.start_chat(enable_automatic_function_calling=True)
 
 def question(pergunta):
